22bec4d9e81c9546e12d0e8b7a8520aee975658f
radgeek <radgeek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-08 10:01:45 +0000

    Initial public offering.
    git-svn-id: https://plugins.svn.wordpress.org/footnotes-for-wordpress/trunk@214873 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,96 @@
+=== Footnotes for WordPress ===
+Contributors: Charles Johnson
+Donate link: http://projects.radgeek.com/wp-footnotes/
+Tags: footnotes, formatting
+Requires at least: 2.9
+Tested up to: 2.9.2
+Stable tag: 2010.0306
+
+Footnotes for WordPress enables easy-to-use fancy footnotes for WordPress posts.
+
+== Description ==
+
+Footnotes for WordPress is a simple plugin designed with a simple aim in mind: to make
+it dead-easy to include decently-formatted footnotes in posts and pages on your
+WordPress blog.
+
+The syntax is roughly based on the common MediaWiki syntax for footnotes, but uses the
+WordPress shortcode conventions. So, to include a footnote with the text "Text," you use:
+
+	This is footnoted.[ref]Text[/ref]
+
+And that's all you need to do. When you add a footnote, Footnotes for WordPress will
+create a note marker at the point that the foonote appears in the text, and includes
+the text of the footnote in a styled list of notes down the page. When a reader clicks
+on the link in a JavaScript-enabled browser, a script included with the plugin will
+create a small bubble inline in the text, which pops up over the footnote marker for
+easy reading without losing their place. In non-JavaScript-enabled contexts, clicking
+on the footnote marker jumps down the page to the text of the note.
+
+The jump is based on an unique anchor which the plugin can automatically generate for
+you. However, if you want to create a specific ID of your own for the footnote, you
+can do so using the `name="..."` attribute:
+
+	This is footnoted.[ref name="my-unique-id-1"]Text[/ref]
+
+By default, the list of footnotes appears at the bottom of the post. However, if you
+wish them to appear somewhere above the bottom of the post, for formatting or other
+reasons, you can do so using the `[references/]` shortcode:
+
+	This is footnoted.[ref]Text[/ref]
+
+	As are some[ref]Text[/ref] other things.
+
+	[references/]
+
+	See also: Endnotes.
+
+Which will cause the two footnotes to appear beneath the second paragraph but above
+the "See also:" text.
+
+== Screenshots ==
+
+1.  Add inline footnotes with dead-simple shortcode syntax.
+
+2.  A note marker with a link to the text of the note will appear next to the footnoted
+    text.
+
+3.  The text of the note(s) will appear down the page.
+
+4.  For ease of reading, users with JavaScript-enabled browsers can read the text of notes
+    inline, without needing to jump down the page or lose their place.
+
+== Installation ==
+
+1.  Upload the `wp-footnotes` directory to the `/wp-content/plugins/` directory of
+    your WordPress installation.
+
+2.  Activate the plugin through the 'Plugins' panel in the WordPress admin interface
+
+3.  Start writing posts with inline footnotes.`[ref]Using this shortcode![/ref]`
+
+== Usage ==
+
+*   To create a footnote, use `[ref]...[/ref]` to wrap the text of your note.
+
+    Example: `This is footnoted.[ref]This is the text of the of the footnote.[/ref]`
+
+*   To create a footnote with a specific unique ID, instead of the one that the plugin
+    will automatically generate for you, use `[ref name="..."]...[/ref]`
+
+    Example: `This is footnoted.[ref name="my-unique-id"]This is the note text.[/ref]`
+
+*   To force the plugin to display notes at a different location instead of at the bottom
+    of the post, use `[references/]`
+
+    Example:
+    	
+	This is footnoted.[ref]Text[/ref]
+
+	As are some[ref]Text[/ref] other things.
+
+	[references/]
+
+	See also: Endnotes.
+
+
---
+++ wp-footnotes.php
@@ -0,0 +1,117 @@
+<?php
+/*
+Plugin Name: Footnotes for WordPress
+Plugin URI: http://projects.radgeek.com/wp-footnotes.php
+Description: easy-to-use fancy footnotes for WordPress posts
+Version: 2010.0306
+Author: Charles Johnson
+Author URI: http://radgeek.com/
+License: GPL
+*/
+
+/**
+ * @package FootnotesForWordPress
+ * @version 2010.0306
+ */
+
+class FootnotesForWordPress {
+	var $accumulated;
+
+	function add_scripts () {
+		$url = str_replace(WP_PLUGIN_DIR, WP_PLUGIN_URL, dirname(__FILE__));
+
+		wp_enqueue_script(
+			'footnote-voodoo',
+			$url.'/footnote-voodoo.js',
+			/*depends on=*/ array('jquery'),
+			/*ver=*/ '2010.0306'
+		);
+		wp_enqueue_style(
+			'footnote-vodoo',
+			$url.'/footnote-voodoo.css',
+			/*depends on=*/ array(),
+			/*ver=*/ '2010.0306'
+		);
+	}
+	function add_inline_styles () {
+	?>
+<style type="text/css">
+
+	.footnote-indicator:before {
+		content: url(<?php print str_replace(WP_PLUGIN_DIR, WP_PLUGIN_URL, dirname(__FILE__)); ?>/footnoted.png);
+		width: 10px;
+		height: 10px;
+	}
+	ol.footnotes li {
+		background: #eeeeee url(<?php print trailingslashit(str_replace(WP_PLUGIN_DIR, WP_PLUGIN_URL, dirname(__FILE__))); ?>note.png) 0px 0px repeat-x;
+	}
+</style>
+<script type="text/javascript">
+	// Globals
+	var tipUpUrl = 'url(<?php print str_replace(WP_PLUGIN_DIR, WP_PLUGIN_URL, dirname(__FILE__)); ?>/tip.png)';
+	var tipDownUrl = 'url(<?php print str_replace(WP_PLUGIN_DIR, WP_PLUGIN_URL, dirname(__FILE__)); ?>/tip-down.png)';
+</script>
+	<?php
+	}
+	function FootnotesForWordPress () {
+		$this->accumulated = array();
+	} /* FootnotesForWordPress constructor */
+
+	function shortcode ($atts, $content = NULL, $code = '') {
+		global $post;
+
+		// Get parameters
+		$atts = shortcode_atts( array(
+			"name" => NULL,
+			'backlink-prefix' => 'to-',
+		), $atts );
+
+		$bullet = (count($this->accumulated) + 1);
+		$noteId = $atts['name'];
+		if (is_null($noteId) and !is_null($post)) :
+			$noteId = $post->post_name.'-n-'.$bullet;
+		endif;
+
+		// Allow any inside shortcodes to do their work.
+		$content = do_shortcode($content);
+		$note_marker = "<strong><sup>[$bullet]</sup></strong>";
+
+		$note = <<<EON
+<li class="footnote" id="$noteId">$note_marker $content <a class="note-return" href="#{$atts['backlink-prefix']}{$noteId}">&#x21A9;</a></li>
+EON;
+		$this->accumulated[] = $note;
+
+		return '<sup>[<a href="#'.$noteId.'" class="footnoted" id="'.$atts['backlink-prefix'].$noteId.'">'.$bullet.'</a>]</sup>';
+	} /* FootnotesForWordPress::shortcode */
+
+	function discharge ($atts, $content = NULL, $code = '') {
+		$notes = '';
+		if (count($this->accumulated) > 0) :
+			$notes = "<ol class=\"footnotes\">\n\t"
+				.implode("\n\t", $this->accumulated)
+				."</ol>\n";
+			$this->accumulated = array();
+		endif;
+
+		return $notes;
+	} /* FootnotesForWordPress::discharge */
+
+	function the_content ($content) {
+		/* Discharge any remaining footnotes */
+		$content .= "\n".$this->discharge();
+
+		return $content;
+	} /* FootnotesForWordPress::the_content() */
+} /* class FootnotesForWordPress */
+
+$footnotesForWordPress = new FootnotesForWordPress;
+
+add_shortcode('ref', array($footnotesForWordPress, 'shortcode'));
+add_shortcode('references', array($footnotesForWordPress, 'discharge'));
+
+// Way downstream; needs to be after do_shortcode (priority 11), for one thing
+add_filter('the_content', array($footnotesForWordPress, 'the_content'), 1000, 2);
+
+add_action('init', array($footnotesForWordPress, 'add_scripts'));
+add_action('wp_head', array($footnotesForWordPress, 'add_inline_styles'));
+
